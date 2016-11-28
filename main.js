897627fd8daeb50a333cb80db40c4b328a54c409
Jeffrey Degoma <jhdegoma@hotmail.com>
2016-11-28 15:11:58 -0500

    just do it

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-console.log("Hi, my name is Chris and I'm 29 years old");
+// console.log("Hi, my name is Chris and I'm 29 years old");
 
-function whoAmI(){
-console.log("Hi, my name is Chris and I'm 29 years old");
-}
+// function whoAmI(){
+// console.log("Hi, my name is Chris and I'm 29 years old");
+// }
 
-whoAmI()
+//whoAmI()
 
 function whoAmI(name, age){
+var yearOfBirth = 2016 - age;
 console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old');
+console.log('I was born in ' + yearOfBirth);
 }
 
 whoAmI('Chris', 29);
