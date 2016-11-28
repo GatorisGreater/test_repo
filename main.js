2a30732de8c3376e26a24d2c7a8be56cb548520b
gatorisgreater <william.broadway@gmail.com>
2016-11-28 15:41:04 -0500

    Is it a habit yet?

--- main.js
+++ main.js
@@ -6,10 +6,37 @@
 
 //whoAmI()
 
+// function whoAmI(name, age){
+// var yearOfBirth = 2016 - age;
+// console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old');
+// console.log('I was born in ' + yearOfBirth);
+// }
+
+// whoAmI('Chris', 29);
+
+// function whoAmI(name, age){
+// var yearOfBirth = yearOfBirth(age);
+// console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old');
+// }
+
+// whoAmI('Chris', 29);
+
+// function yearOfBirth(age) {
+// 	var yearOfBirth = 2016 - age;
+// 	console.log('I was born in ' + yearOfBirth);
+// }
+//This is an example of hoisting
+
 function whoAmI(name, age){
-var yearOfBirth = 2016 - age;
+var yob = yearOfBirth(age);
 console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old');
-console.log('I was born in ' + yearOfBirth);
 }
 
 whoAmI('Chris', 29);
+
+function yearOfBirth(age) {
+	var yearOfBirth = 2016 - age;
+	console.log('I was born in ' + yearOfBirth);
+}
+
+
