5358e23b04bfc8bf6a7a70a722a4c36517ecb51a
gatorisgreater <william.broadway@gmail.com>
2016-11-28 14:58:31 -0500

    Exercise 3 of Set 1 Drills.

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ console.log("Hi, my name is Chris and I'm 29 years old");
 }
 
 whoAmI()
+
+function whoAmI(name, age){
+console.log('Hi, my name is ' + name + ' and I\'m ' + age + ' years old');
+}
+
+whoAmI('Chris', 29);
